a798642c63ca5d27b4bebaa77d97609df55e9cde
Remtori <lqv.remtori@gmail.com>
2020-05-06 16:25:38 +0700

    ASL: make ASSERT call __debugbreak() when running in Visual Studio

--- Libraries/ASL/Assertions.h
+++ Libraries/ASL/Assertions.h
@@ -1,5 +1,11 @@
 #pragma once
 
+
+#ifdef _MSC_VER
+#define ASSERT(x) if(!(x)) { __debugbreak(); assert(false); }
+#else
 #include "assert.h"
 #define ASSERT assert
-#define ASSERT_NOT_REACHED() assert(false)
+#endif
+
+#define ASSERT_NOT_REACHED() ASSERT(false)
