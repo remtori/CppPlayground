50531e223026312ad3e6233fb35f4b9d26ef18cc
Remtori <lqv.remtori@gmail.com>
2020-05-12 14:51:18 +0700

    ASL: TryToString now can actually return String when encounter one

--- Libraries/ASL/TryToString.h
+++ Libraries/ASL/TryToString.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Demangle.h"
+#include "Forward.h"
 #include "StdLibExtras.h"
 #include "String.h"
 #include "StringBuilder.h"
@@ -8,28 +9,34 @@
 namespace ASL {
 
 template<class T>
-constexpr auto call_to_string_if_present(const T* obj, String& result) -> decltype(obj->to_string(), TrueType {})
+constexpr auto call_to_string_if_present(const T& obj, String& result) -> decltype(obj.to_string(), TrueType {})
 {
-    result = obj->to_string();
+    result = obj.to_string();
     return {};
 }
 
-constexpr auto call_to_string_if_present(...) -> FalseType
-{
-    return {};
-}
+constexpr auto call_to_string_if_present(...) -> FalseType { return {}; }
 
 template<typename T>
 String try_to_string(const T& any)
 {
+    if constexpr (CanConstructString<T>::value)
+        return any;
+
+    String result;
+    if constexpr (IsPointer<T>::value)
+        call_to_string_if_present(*any, result);
+    else
+        call_to_string_if_present(any, result);
+
+    if (!result.is_empty())
+        return result;
+
     StringBuilder builder(96);
     builder.append("<error>class ");
     builder.append(demangle(typeid(T).name()));
     builder.append(" is missing ::to_string() method</error>");
-    String result = builder.to_string();
-
-    call_to_string_if_present(&any, result);
-    return result;
+    return builder.to_string();
 }
 
 } // namespace ASL
