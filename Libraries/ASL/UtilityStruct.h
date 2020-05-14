6ec312a65688f81411e5300923891112633f3e34
Remtori <lqv.remtori@gmail.com>
2020-05-14 14:42:49 +0700

    ASL: Add UtilityStruct for commonly use struct like Position, Size and Rect

---
+++ Libraries/ASL/UtilityStruct.h
@@ -0,0 +1,39 @@
+#pragma once
+
+namespace ASL {
+
+template<typename T>
+struct BasePosition {
+    T x = 0;
+    T y = 0;
+};
+
+template<typename T>
+struct BaseSize {
+    T x = 0;
+    T y = 0;
+};
+
+template<typename T>
+struct BaseRect {
+    T x = 0;
+    T y = 0;
+    T x = 0;
+    T y = 0;
+};
+
+using Position = BasePosition<int>;
+using FPosition = BasePosition<float>;
+using Size = BaseSize<int>;
+using FSize = BaseSize<float>;
+using Rect = BaseRect<int>;
+using FRect = BaseRect<float>;
+
+} // namespace ASL
+
+using ASL::FPosition;
+using ASL::FRect;
+using ASL::FSize;
+using ASL::Position;
+using ASL::Rect;
+using ASL::Size;
