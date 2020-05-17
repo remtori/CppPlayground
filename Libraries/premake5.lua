122400130d971c78972d77e762df21e7b73b2886
Remtori <lqv.remtori@gmail.com>
2020-05-17 15:02:11 +0700

    Toolchain: Reorder project to alphabetical order

--- Libraries/premake5.lua
+++ Libraries/premake5.lua
@@ -1,16 +1,8 @@
 make_proj('ASL', './ASL')
 	filter "system:windows"
 		links { 'Dbghelp' }
-
 	make_proj('TestASL', './ASL/Tests', 'Test')
 		links { 'ASL' }
-
-make_proj('GUI', './LibGUI')
-	links { 'ASL' }
-	filter "system:linux"
-		links { 'X11' }
-		files { './LibGUI/Platform/X11/*.h', './LibGUI/Platform/X11/*.cpp' }
-
 
 make_proj('Codec', './LibCodec')
 	links { 'ASL', 'Crypto' }
@@ -21,3 +13,12 @@ make_proj('Crypto', './LibCrypto')
 	links { 'ASL' }
 	make_proj('TestCrypto', './LibCrypto/Tests', 'Test')
 		links { 'ASL', 'Crypto' }
+
+
+make_proj('GUI', './LibGUI')
+	links { 'ASL' }
+	filter "system:linux"
+		links { 'X11' }
+		files { './LibGUI/Platform/X11/*.h', './LibGUI/Platform/X11/*.cpp' }
+	filter "system:windows"
+		files { './LibGUI/Platform/Windows/*.h', './LibGUI/Platform/Windows/*.cpp' }
