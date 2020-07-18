285a0559de874d54b684ac4fae62cb12e97c92f9
Remtori <lqv.remtori@gmail.com>
2020-07-18 14:28:55 +0700

    ASL: Remove HANDLE_CRASH() Macro

--- Libraries/ASL/Backtrace.h
+++ Libraries/ASL/Backtrace.h
@@ -1,17 +1,5 @@
 #pragma once
 
-#include <ASL/Platform.h>
-
-#ifdef PLATFORM_LINUX
-#    include <signal.h>
-#elif defined(PLATFORM_WINDOWS)
-#endif
-
-#define HANDLE_CRASH()                     \
-    signal(SIGABRT, ASL::print_backtrace); \
-    signal(SIGSEGV, ASL::print_backtrace); \
-    signal(SIGTRAP, ASL::print_backtrace);
-
 namespace ASL {
 void print_backtrace(int signal);
 } // namespace ASL
--- Libraries/ASL/Init.h
+++ Libraries/ASL/Init.h
@@ -2,12 +2,24 @@
 
 #include <ASL/Backtrace.h>
 #include <ASL/Logger.h>
+#include <ASL/Platform.h>
+
+#ifdef PLATFORM_LINUX
+#    include <signal.h>
+#elif defined(PLATFORM_WINDOWS)
+#endif
 
 namespace ASL {
 
 void initialize(const char* arg0)
 {
-    HANDLE_CRASH();
+#ifdef PLATFORM_LINUX
+    signal(SIGABRT, ASL::print_backtrace);
+    signal(SIGSEGV, ASL::print_backtrace);
+    signal(SIGTRAP, ASL::print_backtrace);
+#else
+#endif
+
     Logger::set_process_name(arg0);
 }
 
