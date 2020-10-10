d5780e0e1954b25fbc3565c58b358d0d8932e054
remtori <lqvu99@gmail.com>
2020-10-10 23:32:38 +0700

    ASL: Attributes.h Macro for compiler attribute

---
+++ Libraries/ASL/Attributes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <ASL/Platform.h>
+
+#ifdef PLATFORM_WINDOWS
+#    ifdef COMPILING_DLL
+#        define LIB_API __declspec(dllexport)
+#    else
+#        define LIB_API __declspec(dllimport)
+#    endif
+#else
+#    define LIB_API
+#endif
+
+#define NO_DISCARD [[nodiscard]]
+#define ALWAYS_INLINE [[always_inline]]
